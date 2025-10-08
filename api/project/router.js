1eb1141bb4f3ccde668aaee9a8968cbedf3f2e4e
Jason Vann <vannjt1@icloud.com>
2025-10-08 17:52:44 -0400

    project router

--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,35 @@
 // build your `/api/projects` router here
 const express = require('express');
+const Project = require("./model");
+
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const projects = await Project.getAll()
+        // Convert project.completed from integer to boolean
+        const formattedProjects = projects.map(project => ({
+            ...project,
+            project_completed: Boolean(project.project_completed)
+        }))
+        res.json(formattedProjects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const project = await Project.create(req.body)
+        // Convert project_completed from integer to boolean
+        const formattedProject = {
+            ...project,
+            project_completed: Boolean(project.project_completed)
+        }
+        res.status(201).json(formattedProject)
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router;
