771856a9f20a0ada3f309413baa39350a57e55b4
Jason Vann <vannjt1@icloud.com>
2025-10-08 20:04:17 -0400

    resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
 // build your `Resource` model here
+const db = require("../../data/dbConfig")
+
+const getAll = () => {
+    return db("resources")
+}
+
+const getById = (resource_id) => {
+    return db("resources").where("resource_id", resource_id).first()
+}
+
+const create = async (resouce) => {
+    const [resource_id] = await db("resources").insert(resouce)
+    return getById(resource_id)
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+}
