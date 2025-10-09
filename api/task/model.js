11483f74e67eada851a5735b38ecfb9851b81c23
Jason Vann <vannjt1@icloud.com>
2025-10-09 17:15:58 -0400

    task model

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,30 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig")
+
+const getAll = () => {
+    return db("tasks as t")
+    .join("projects as p", "t.project_id", "p.project_id")
+    .select(
+        "t.task_id",
+        "t.task.description",
+        "t.task_notes",
+        "t.task_completed",
+        "p.project_name",
+        "p.project_description"
+    )
+}
+
+const getById = (task_id) => {
+    return db("tasks").where("task_id", task_id).first()
+}
+
+const create = async (task) => {
+    const [task_id] = await db("tasks").insert(task)
+    return getById(task_id)
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+}
