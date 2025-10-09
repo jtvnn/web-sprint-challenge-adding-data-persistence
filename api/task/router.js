8f4322e923fcb237770479bdd707b6cd92f9cf8d
Jason Vann <vannjt1@icloud.com>
2025-10-09 18:11:21 -0400

    Task router

--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,34 @@
-// build your `/api/tasks` router here
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const Task = require('./model')
 
-module.exports = router;
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const tasks = await Task.getAll()
+    // Convert task_completed from integer to boolean
+    const formattedTasks = tasks.map(task => ({
+      ...task,
+      task_completed: Boolean(task.task_completed)
+    }))
+    res.json(formattedTasks)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const task = await Task.create(req.body)
+    // Convert task_completed from integer to boolean
+    const formattedTask = {
+      ...task,
+      task_completed: Boolean(task.task_completed)
+    }
+    res.status(201).json(formattedTask)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
