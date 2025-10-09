173c95f515168c5ac1a99756c6563c41d159fb42
Jason Vann <vannjt1@icloud.com>
2025-10-09 16:36:33 -0400

    resouce router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,25 @@
 // build your `/api/resources` router here
 const express = require('express');
+const Resource = require("./model");
+
 const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    try {
+        const resources = await Resource.getAll()
+        res.json(resouces)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const resource = await Resouce.create(req.body)
+        res.status(201).json(resource)
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router;
